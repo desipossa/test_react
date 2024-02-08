68e502a8f2a035620a81ff1aa93ee41a66a4b1ac
desipossa <c1jang@hanmail.net>
2024-02-08 13:09:40 +0900

    20240208

--- src/MainVisual.js
+++ src/MainVisual.js
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
+
+import "./MainVisual.scss";
 
 const MainVisual = () => {
 
     const [slideNum, setSlideNUm] = useState(0);
 
+    const slide = useRef(null);
+
     const option = {
         arrows: false,
-        dots: true,
+        //dots: true,
         afterChange: (idx) => {
             setSlideNUm(idx);
         }
@@ -22,12 +26,12 @@ const MainVisual = () => {
         { tit: "title05", desc: "아무꺼나 씁니다. 05" },
     ]
     return (
-        <>
-            <Slider {...option} className="Main_visual_slide">
+        <div className="MainVisual">
+            <Slider {...option} className="Main_visual_slide" ref={slide}>
                 {
                     slideData.map((it, idx) => {
                         return (
-                            <div className="itm">
+                            <div className={`itm itm0${idx + 1} ${idx == slideNum ? "on" : ""}`}>
                                 <div className="slg">
                                     <strong>{it.tit}</strong>
                                     <p>
@@ -42,7 +46,23 @@ const MainVisual = () => {
             <div className="num">
                 {slideNum + 1} / {slideData.length}
             </div>
-        </>
+            <div className="arrows">
+                <button className="prev" onClick={() => slide.current.slickPrev()}>prev</button>
+                <button className="next" onClick={() => slide.current.slickNext()}>next</button>
+            </div>
+            <ul className="dots">
+                {
+                    slideData.map((it, idx) => {
+                        return (
+                            <li className={`${idx == slideNum ? "on" : ""}`}>
+                                <button onClick={() => slide.current.slickGoTo(idx)}>{idx + 1}</button>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            {console.log(slide.current)}
+        </div>
     )
 }
 
